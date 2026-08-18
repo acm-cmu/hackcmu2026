fb0eb065a4a181ebd04786e35d25327d146f668a
katie0014 <katiewang314@gmail.com>
2026-08-18 11:16:44 -0400

    renamed schedule events to fit the train theme
    Check-In -> Boarding, Opening Ceremony -> Departure, Closing Ceremony -> Arrival

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -7,13 +7,13 @@ const SCHEDULE = [
     events: [
       {
         time: '5:00 – 6:00 pm',
-        title: 'Check-In',
+        title: 'Boarding',
         location: 'Tepper Simmons Auditorium',
         desc: 'Check in with organizers and find your team members.',
       },
       {
         time: '6:00 – 6:30 pm',
-        title: 'Opening Ceremony',
+        title: 'Departure',
         location: 'Tepper Simmons Auditorium',
         desc: 'Join us for the opening ceremony of HackCMU 2026!',
       },
@@ -60,7 +60,7 @@ const SCHEDULE = [
       },
       {
         time: '7:00 – 8:00 pm',
-        title: 'Closing Ceremony',
+        title: 'Arrival',
         location: 'Tepper Simmons Auditorium',
         desc: 'We hope you continue to reach for the stars!',
       },
