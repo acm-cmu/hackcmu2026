2d729eb350840516bd3358296d483de4bc015595
katie0014 <katiewang314@gmail.com>
2026-09-02 09:42:50 -0400

    added Quadrature's custom sponsor popup message

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -148,7 +148,27 @@ const SPONSOR_LIST = [
     ),
   },
   { name: 'Microsoft', logo: microsoftLogo },
-  { name: 'Quadrature', logo: quadratureLogo },
+  {
+    name: 'Quadrature',
+    logo: quadratureLogo,
+    blurb: (
+      <>
+        <p>Quadrature is a valued sponsor of ACM@CMU.</p>
+        <p>This is a message from them:</p>
+        <p>
+          Founded by programmers in 2010, we’re a systematic trading firm made up of smart,
+          collaborative people who are passionate about using technology to solve complex
+          problems. By combining cutting-edge technology with deep quantitative research, we
+          build automated trading systems that generate consistent, significant returns on our
+          proprietary capital.
+        </p>
+        <p>
+          At Quadrature, we envision a future where we trade all liquid, electronically tradeable
+          asset classes across all horizons.
+        </p>
+      </>
+    ),
+  },
   {
     name: 'Querit',
     logo: queritLogo,
